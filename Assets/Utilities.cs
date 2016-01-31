7066eee0429b22297b8ab9e0c6b239c6257db02e
Greg-Rus <grzegorz.rus@gmail.com>
2016-01-31 11:17:08 +0100

    Initial Commit

---
+++ Assets/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utilities : MonoBehaviour {
+
+	public static Quaternion rotationFromForwardToVector(Vector3 vector)
+	{
+		float angle = Mathf.Atan2(vector.y,vector.x) * Mathf.Rad2Deg;
+		return Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+	public static bool RollBelowPercent(float pecent)
+	{
+		float roll = UnityEngine.Random.Range (0f,1f);
+		if(roll <= pecent)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+}
