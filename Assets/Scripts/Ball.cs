550cc200c3825149d3b1eed4dcc743cdc1cfe64d
Greg-Rus <grzegorz.rus@gmail.com>
2016-02-11 20:20:07 +0100

    Fuckup Restore

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,12 +8,15 @@ public class Ball : MonoBehaviour {
 	public float gravityModifier = 0.1f;
 	public float velocityMag;
 	public float correctiveAcceleration = 300f;
+	public GameObject Marker;
 	Rigidbody2D myRigidBody;
+	private Vector3 startPosition;
 	// Use this for initialization
 	void Start () {
 		direction = Vector3.up;
 		myRigidBody = GetComponent<Rigidbody2D>();
-		myRigidBody.AddRelativeForce(Vector3.right * speed);
+		//myRigidBody.AddRelativeForce(Vector3.right * speed);
+		startPosition = this.transform.position;
 	}
 	
 	// Update is called once per frame
@@ -29,6 +32,33 @@ public class Ball : MonoBehaviour {
 		{
 			myRigidBody.AddRelativeForce(myRigidBody.velocity * -correctiveAcceleration);
 		}
+		if(!GameController.instance.gamePaused && GameController.instance.gameStarted)
+		{
+			if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+			{
+				Marker.SetActive(true);
+			}
+			if(Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+			{
+				Marker.SetActive(false);
+			}
+		}
+
+	}
+	
+	public void SetGravityScale(float gravity)
+	{
+		myRigidBody.gravityScale = gravity;
+	}
+	
+	public void SetVelocity(Vector3 velocity)
+	{
+		myRigidBody.velocity = velocity;
+	}
+	
+	public void DisplayMarker( Vector2 direction)
+	{
+		Marker.transform.rotation = Utilities.rotationFromForwardToVector(new Vector3(direction.x, direction.y, 0f));
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll) {
@@ -52,4 +82,14 @@ public class Ball : MonoBehaviour {
 		}
 	
 	}
+	public void Reset()
+	{
+		myRigidBody.isKinematic = true;
+		myRigidBody.transform.position = startPosition;
+		myRigidBody.isKinematic = false;
+		//myRigidBody.velocity = Vector3.zero;
+		//myRigidBody.gravityScale = 2f;
+		
+	}
+
 }
